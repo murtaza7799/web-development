b5ed80471111dd1d16e56b249d9cd374927a9a07
Muhammad Murtaza <48488384+murtaza7799@users.noreply.github.com>
2021-10-25 12:34:36 -0700

    main

--- AjexGetRequest/index.js
+++ AjexGetRequest/index.js
@@ -1,12 +1,25 @@
 $(function(){
- $("#mybtn").click(sendAjex);
+    //simplified
+ $("#mybtn").click(function(){
+     $.get("student.txt",function(e){
+        $("#result").empty();
+        $("#result").text(e);
+     })
+ });
 
 
+// check ajex function and check response function and its call 
 })
 function sendAjex(){
-    console.log("Sending Ajex Call to server")
+    console.log("Sending Ajex Call to server");
+    $.get("student.txt",handelResponce);
 }
 function handelResponce(response){
+    console.log("responce received");
+    console.log(response);
+
+    $("#result").empty();
+    $("#result").text(response);
 
 
 
